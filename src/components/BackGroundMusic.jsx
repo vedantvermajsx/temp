9c71211e4a3744aadac58e27bc9bdd40280f5529
creepJxvedant <vedantverma303@gmail.com>
2025-02-14 23:57:10 +0530

    last

--- src/components/BackGroundMusic.jsx
+++ src/components/BackGroundMusic.jsx
@@ -24,7 +24,7 @@ const BackgroundMusic = () => {
   return (
     <div className="flex items-center will-change-contents p-2 justify-center mt-4 bg-gray-100/5 rounded-full shadow-lg gap-4">
       <audio ref={audioRef} loop>
-        <source src="../../public/music/des mots.mp3" type="audio/mp3" />
+        <source src="../../public/des mots.mp3" type="audio/mp3" />
         Your browser does not support the audio element.
       </audio>
       <button
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// filepath: /b:/development/portfolio/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
